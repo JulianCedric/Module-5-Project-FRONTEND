48c347f31d7e464075f32173bc524713076c2757
JulianCedric <julian.pormentilla@gmail.com>
2020-08-27 15:31:39 -0400

    Updates to ProgressBarsContainer

--- src/ProgressBarsContainer.js
+++ src/ProgressBarsContainer.js
@@ -6,49 +6,53 @@ class ProgressBarsContainer extends React.Component {
     render() { 
         return (  
 
-                <div>
-                    <h2>Progress Bars Container</h2>
-                    <p></p>
-                    <br></br>
+            <div>
+                <h2>Progress Bars Container</h2>
+                <p></p>
+                <br></br>
+                
+                <div class="ui center aligned three column grid">
+
+                        <div class="column">
+                            <div class="ui segment">
+                                <h2>Habit 1</h2>
+                                <ProgressBar className="progress-bar"
+                                    progress={25}
+                                    radius={100}
+                                    strokeColor='violet'
+                                    strokeWidth={20}
+                                />
+                            </div>
+                        </div>
+                        <div class="column">
+                            <div class="ui segment">
+                                <h2>Habit 2</h2>
+                                <ProgressBar className="progress-bar"
+                                    progress={50}
+                                    radius={100}
+                                    strokeColor='orange'
+                                    strokeWidth={20}
+                                    // pointerRadius={5}
+                                    // pointerStrokeWidth={15}	
+                                    // pointerStrokeColor='white'	
+                                />
+                            </div>
+                        </div>
+                        <div class="column">
+                            <div class="ui segment">
+                                <h2>Habit 3</h2>
+                                <ProgressBar className="progress-bar"
+                                    progress={75}
+                                    radius={100}
+                                    strokeColor='blue'
+                                    strokeWidth={20}
+                                />
+                            </div>       
+                        </div>
+
                 </div>
 
-
-
-
-
-                <div class="ui three column centered grid">
-
-                        <div class="column">
-                            <h2>test_column1</h2>
-                            <ProgressBar 
-                                progress={50}
-                                radius={100}
-                                strokeColor='purple'
-                                strokeWidth={30}
-                            />
-                        </div>
-                        <div class="column">
-                            <h2>test_column2</h2>
-                            <ProgressBar 
-                                progress={50}
-                                radius={100}
-                                strokeColor='orange'
-                                strokeWidth={30}
-                            />
-                        </div>
-                        <div class="column">
-                            <h2>test_column3</h2>
-                            <ProgressBar 
-                                progress={50}
-                                radius={100}
-                                strokeColor='orange'
-                                strokeWidth={30}
-                            />
-                        </div>
-                    
-
-
-                </div>
+            </div>
 
         );
     }
