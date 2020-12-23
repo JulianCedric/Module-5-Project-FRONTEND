669715b30f4ec99099fea957bff01ef13c229446
JulianCedric <julian.pormentilla@gmail.com>
2020-12-22 22:29:48 -0500

    Updates to Lab

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css'
+import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
 import Login from './Login';
--- src/Lab.js
+++ src/Lab.js
@@ -1,14 +1,86 @@
 import React from 'react';
+import './App.css';
+import {
+  Button,
+  Card,
+  Divider,
+  Grid,
+  Header,
+  Icon,
+  Search,
+  Segment,
+} from 'semantic-ui-react';
 
 class Lab extends React.Component {
     state = {  }
     render() { 
         return (  
             <div style={{color: 'white'}}>
-                <h1>Lab</h1>
+                <Segment placeholder>
+                    <Grid columns={1} stackable textAlign='center'>
+                        <Grid.Row verticalAlign='middle'>
+                            <Grid.Column>
+                                <Header icon>
+                                    <Icon name='lab' />
+                                        <h1>Lab</h1>
+                                </Header>
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
+                </Segment>
 
-                
-
+                <div className="four-step-method">
+                    <Segment inverted style={{width: '555px'}}>
+                        <Grid columns={2} stackable textAlign='center'>
+                            <Divider vertical/>
+                            <Grid.Row verticalAlign='middle'>
+                                <Grid.Column>
+                                    <Card color='violet'>
+                                        <Card.Content>
+                                            <Card.Header>
+                                                Problem Phase
+                                            </Card.Header>
+                                        </Card.Content>
+                                        <Card.Content extra>
+                                            <div className='ui two buttons'>
+                                                <Button inverted color='violet'>
+                                                    CUE
+                                                </Button>
+                                                <Button inverted color='violet'>
+                                                    CRAVING
+                                                </Button>
+                                            </div>
+                                        </Card.Content>
+                                    </Card>
+                                </Grid.Column>
+                                <Grid.Column>
+                                    <Card color='violet'>
+                                        <Card.Content>
+                                            <Card.Header>
+                                                Solution Phase
+                                            </Card.Header>
+                                        </Card.Content>
+                                        <Card.Content extra>
+                                            <div className='ui two buttons'>
+                                                <Button inverted color='violet'>
+                                                    RESPONSE
+                                                </Button>
+                                                <Button inverted color='violet'>
+                                                    REWARD
+                                                </Button>
+                                            </div>
+                                        </Card.Content>
+                                    </Card>
+                                </Grid.Column>
+                            </Grid.Row>                            
+                        </Grid>
+                    </Segment>
+                </div>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
             </div>
         );
     }
