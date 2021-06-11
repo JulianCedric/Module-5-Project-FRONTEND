4d4a7b399e06b61c26dcb7c857398e907d73ef3f
JulianCedric <julian.pormentilla@gmail.com>
2021-06-11 18:13:15 -0400

    Updates to Home

--- src/Home.js
+++ src/Home.js
@@ -3,30 +3,7 @@ import React from 'react';
 class Home extends React.Component {
     render() { 
         return (  
-            <div className="home-container">
-                
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                
+            <div className="home-container"> 
             </div>
         );
     };
--- src/Signup.js
+++
@@ -1,2 +0,0 @@
-import React from 'react';  
-import { Form, Grid } from 'semantic-ui-react';
