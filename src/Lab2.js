68d8a9a74608f7d30197dcfc22bab7aba6ea2846
JulianCedric <julian.pormentilla@gmail.com>
2021-12-09 16:37:31 -0500

    Lab2 updates

--- src/Lab2.js
+++ src/Lab2.js
@@ -4,10 +4,8 @@ class Lab2 extends React.Component {
     render() { 
         return 
             <div>
-                <h1>Test</h1>
             </div>;
     }
 }
  
 export default Lab2;
-    
