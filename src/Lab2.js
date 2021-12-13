3c74caab84b1404d3f10055031a3137740f2df68
JulianCedric <julian.pormentilla@gmail.com>
2021-12-13 13:37:16 -0500

    Created handleToggle function in Lab2

--- src/Lab2.js
+++ src/Lab2.js
@@ -1,13 +1,5 @@
-/* 
-Users
-Goals
-Tasks
-Quotes
-*/
-
 import React, { useState } from 'react';
 import Goals from './Goals';
-import Tasks from './Tasks';
 import Quotes from './Quotes';
 import CreateTask from './CreateTask';
 import CreateQuote from './CreateQuote';
@@ -18,22 +10,20 @@ import ToDoList from './ToDoList';
 function Lab2 () {
     const [ toDoList, setToDoList ] = useState(data);
 
-        return (
-            <div className="lab2Container">
-                <h1>Lab2</h1>
-                <Header />
-                <ToDoList />
-                <div className="lab2">
-                    <Goals/>
-                    <Tasks/>
-                    <CreateTask/>
-                    <Quotes/>
-                    <div className='sidebar'>
-                        {(this.state.showQuotes ? <CreateQuote/> : null)}
-                    </div>
-                </div>
-            </div>
-        )
+    const handleToggle = (id) => {
+        let mapped = toDoList.map(task => {
+            return task.id == id ? { ...task, complete: !task.complete } : { ...task};
+        });
+        setToDoList(mapped);
+    }
+
+    return (
+        <div className="lab2Container">
+            <h1>Lab2</h1>
+            <Header />
+            <ToDoList toDoList={toDoList} />
+        </div>
+    )
 }
  
 export default Lab2; 
