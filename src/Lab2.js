5735fd1f2942718700c74aaeb721776598af7e1b
JulianCedric <julian.pormentilla@gmail.com>
2021-12-13 14:18:04 -0500

    ToDoForm complete

--- src/Lab2.js
+++ src/Lab2.js
@@ -6,6 +6,7 @@ import CreateQuote from './CreateQuote';
 import Header from './Header';
 import data from './data.json';
 import ToDoList from './ToDoList';
+import ToDoForm from './ToDoForm';
 
 function Lab2 () {
     const [ toDoList, setToDoList ] = useState(data);
@@ -25,13 +26,20 @@ function Lab2 () {
         setToDoList(filtered);
     }
 
+    const addTask = (userInput) => {
+        let copy = [...toDoList];
+        copy = [...copy, { id: toDoList.length + 1, task: userInput, complete: false }];
+        setToDoList(copy);
+    }   
+
     return (
         <div className="lab2Container">
             <h1>Lab2</h1>
             <Header />
             <ToDoList toDoList={toDoList} handleToggle={handleToggle} handleFilter={handleFilter}/>
+            <ToDoForm addTask={addTask}/>
         </div>
     )
 }
  
-export default Lab2; 
+export default Lab2;
